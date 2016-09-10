21c09f616ee86913b255bf6e70d20dc56f31d785
bunsim <bunsim@protonmail.com>
2016-09-10 10:30:06 -0400

    match niaucchi's new API, sane rate limit

--- entry/doForward.go
+++ entry/doForward.go
@@ -1,13 +1,11 @@
 package entry
 
 import (
-	"context"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"time"
-
-	"golang.org/x/time/rate"
 
 	"github.com/bunsim/kiss"
 )
@@ -19,10 +17,6 @@ func (cmd *Command) doForward(lsnr net.Listener, cookie []byte, dest *string) {
 			return
 		}
 		go func() {
-			// rate limit every connection in the long run to 16 KiB/s
-			ulmt := rate.NewLimiter(16*1024, 512*1024)
-			dlmt := rate.NewLimiter(16*1024, 512*1024)
-			ctx := context.Background()
 			defer raw.Close()
 			clnt, err := kiss.LLObfsServerHandshake(cookie, raw)
 			if err != nil {
@@ -38,31 +32,9 @@ func (cmd *Command) doForward(lsnr net.Listener, cookie []byte, dest *string) {
 			go func() {
 				defer remote.Close()
 				defer clnt.Close()
-				buf := make([]byte, 16384)
-				for {
-					n, err := remote.Read(buf)
-					if err != nil {
-						return
-					}
-					dlmt.WaitN(ctx, n)
-					_, err = clnt.Write(buf[:n])
-					if err != nil {
-						return
-					}
-				}
+				io.Copy(clnt, remote)
 			}()
-			buf := make([]byte, 16384)
-			for {
-				n, err := clnt.Read(buf)
-				if err != nil {
-					return
-				}
-				ulmt.WaitN(ctx, n)
-				_, err = remote.Write(buf[:n])
-				if err != nil {
-					return
-				}
-			}
+			io.Copy(remote, clnt)
 		}()
 	}
 }
--- exit/proxy.go
+++ exit/proxy.go
@@ -1,10 +1,13 @@
 package exit
 
 import (
+	"context"
 	"io"
 	"log"
 	"net"
 	"time"
+
+	"golang.org/x/time/rate"
 
 	"github.com/bunsim/niaucchi"
 )
@@ -28,51 +31,85 @@ func (cmd *Command) doProxy() {
 
 	log.Println("niaucchi listening on port 2378")
 	for {
-		clnt, err := lsnr.Accept()
+		ss, err := lsnr.AcceptSubstrate()
 		if err != nil {
 			panic(err.Error())
 		}
 		go func() {
-			defer clnt.Close()
-			// pascal string of the address
-			lb := make([]byte, 1)
-			_, err := io.ReadFull(clnt, lb)
-			if err != nil {
-				return
-			}
-			addrbts := make([]byte, lb[0])
-			_, err = io.ReadFull(clnt, addrbts)
-			if err != nil {
-				return
-			}
-			log.Println("requested to proxy", string(addrbts))
-			// resolve and connect
-			addr, err := net.ResolveTCPAddr("tcp", string(addrbts))
-			if err != nil {
-				return
-			}
-			// block connections to things in the CIDR blacklist
-			for _, n := range cidrBlacklist {
-				if n.Contains(addr.IP) {
-					log.Println("refusing to connect to local address", addr)
+			// per-substrate rate limit of 96 KiB/s
+			limit := rate.NewLimiter(rate.Limit(96*1024), 512*1024)
+			ctx := context.Background()
+			for {
+				defer ss.Tomb().Kill(io.ErrClosedPipe)
+				clnt, err := ss.AcceptConn()
+				if err != nil {
 					return
 				}
+				go func() {
+					defer clnt.Close()
+					// pascal string of the address
+					lb := make([]byte, 1)
+					_, err := io.ReadFull(clnt, lb)
+					if err != nil {
+						return
+					}
+					addrbts := make([]byte, lb[0])
+					_, err = io.ReadFull(clnt, addrbts)
+					if err != nil {
+						return
+					}
+					log.Println("requested to proxy", string(addrbts))
+					// resolve and connect
+					addr, err := net.ResolveTCPAddr("tcp", string(addrbts))
+					if err != nil {
+						return
+					}
+					// block connections to things in the CIDR blacklist
+					for _, n := range cidrBlacklist {
+						if n.Contains(addr.IP) {
+							log.Println("refusing to connect to local address", addr)
+							return
+						}
+					}
+					// go ahead and connect
+					log.Println("proxying", addr)
+					rmt, err := net.DialTimeout("tcp", addr.String(), time.Second*5)
+					if err != nil {
+						log.Println("failed to connect to", addr)
+						return
+					}
+					// forward traffic
+					defer rmt.Close()
+					go func() {
+						defer rmt.Close()
+						defer clnt.Close()
+						buf := make([]byte, 16384)
+						for {
+							n, err := rmt.Read(buf)
+							if err != nil {
+								return
+							}
+							limit.WaitN(ctx, n)
+							_, err = clnt.Write(buf[:n])
+							if err != nil {
+								return
+							}
+						}
+					}()
+					buf := make([]byte, 16384)
+					for {
+						n, err := clnt.Read(buf)
+						if err != nil {
+							return
+						}
+						limit.WaitN(ctx, n)
+						_, err = rmt.Write(buf[:n])
+						if err != nil {
+							return
+						}
+					}
+				}()
 			}
-			// go ahead and connect
-			log.Println("proxying", addr)
-			rmt, err := net.DialTimeout("tcp", addr.String(), time.Second*5)
-			if err != nil {
-				log.Println("failed to connect to", addr)
-				return
-			}
-			// forward traffic
-			defer rmt.Close()
-			go func() {
-				defer rmt.Close()
-				defer clnt.Close()
-				io.Copy(rmt, clnt)
-			}()
-			io.Copy(clnt, rmt)
 		}()
 	}
 }

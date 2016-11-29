bf6dab6ab7dd817a13cdf693efa19d9fcbeb7deb
bunsim <bunsim@protonmail.com>
2016-11-29 11:51:47 -0500

    memoized iptocountry

--- binder/ipToCountry.go
+++ binder/ipToCountry.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -16,7 +17,20 @@ var cleanHTTP = &http.Client{
 	Timeout: time.Second * 10,
 }
 
+var ipcache = make(map[string]struct {
+	cntry string
+	exp   time.Time
+})
+var ipcachelk sync.Mutex
+
 func ipToCountry(ip string) (reg string, err error) {
+	ipcachelk.Lock()
+	cr, ok := ipcache[ip]
+	ipcachelk.Unlock()
+	if ok && cr.exp.After(time.Now()) {
+		reg = cr.cntry
+		return
+	}
 	resp, err := cleanHTTP.Get(fmt.Sprintf("https://freegeoip.net/json/%v", ip))
 	if err != nil {
 		return
@@ -31,5 +45,31 @@ func ipToCountry(ip string) (reg string, err error) {
 	}
 	err = json.NewDecoder(resp.Body).Decode(&jsxp)
 	reg = jsxp.CountryCode
+	ipcachelk.Lock()
+	ipcache[ip] = struct {
+		cntry string
+		exp   time.Time
+	}{reg, time.Now().Add(time.Hour * 24)}
+	ipcachelk.Unlock()
 	return
+}
+
+func init() {
+	go func() {
+		// garbage collect
+		for {
+			time.Sleep(time.Hour)
+			var todel []string
+			ipcachelk.Lock()
+			for k, v := range ipcache {
+				if v.exp.After(time.Now()) {
+					todel = append(todel, k)
+				}
+			}
+			for _, td := range todel {
+				delete(ipcache, td)
+			}
+			ipcachelk.Unlock()
+		}
+	}()
 }

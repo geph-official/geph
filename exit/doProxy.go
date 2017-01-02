2fd6184fa537ded1dda02a5cd41e11bae6a4e690
bunsim <bunsim@protonmail.com>
2017-01-02 11:46:50 -0500

    niaucchi2 support in exit

--- exit/doProxy.go
+++ exit/doProxy.go
@@ -1,15 +1,64 @@
 package exit
 
 import (
+	"encoding/base32"
 	"io"
+	"log"
+	"math/rand"
 	"net"
+	"strings"
 	"sync"
+
+	"golang.org/x/time/rate"
 
 	"gopkg.in/bunsim/natrium.v1"
 
 	"github.com/bunsim/geph/niaucchi2"
 	"github.com/bunsim/miniss"
 )
+
+func (cmd *Command) manageOneCtx(uid string, nct *niaucchi2.Context) {
+	defer nct.Tomb().Kill(io.ErrClosedPipe)
+	// check balance first
+	bal, err := cmd.decAccBalance(uid, 0)
+	if err != nil {
+		log.Println("error authenticating user", uid, ":", err)
+	} else {
+		log.Println(uid, "connected with", bal, "MiB left")
+	}
+	limit := rate.NewLimiter(rate.Limit(cmd.bwLimit*1024), 512*1024)
+	harshlimit := rate.NewLimiter(rate.Limit(32*1024), 128*1024)
+	// little balance
+	lbal := 0
+	var lblk sync.Mutex
+	// consume bytes, returns true if succeeds, otherwise returns false and kills everything
+	consume := func(dec int) bool {
+		lblk.Lock()
+		defer lblk.Unlock()
+		lbal -= dec
+		if lbal <= 0 {
+			num := rand.Int()%10 + 5
+			bal, err := cmd.decAccBalance(uid, num)
+			if err != nil || bal == 0 {
+				return false
+			}
+			lbal += 1000 * 1000 * num
+		}
+		return true
+	}
+	// at the very end, return the small balance
+	defer func() {
+		cmd.decAccBalance(uid, -lbal/1000000)
+	}()
+	// Accept loop
+	for {
+		clnt, err := nct.Accept()
+		if err != nil {
+			return
+		}
+		go cmd.proxyCommon(consume, limit, harshlimit, uid, clnt)
+	}
+}
 
 // doProxy using the new protocol
 func (cmd *Command) doProxy() {
@@ -33,6 +82,10 @@ func (cmd *Command) doProxy() {
 				wire.Close()
 				return
 			}
+			pub := mwire.RemotePK()
+			uid := strings.ToLower(
+				base32.StdEncoding.EncodeToString(
+					natrium.SecureHash(pub, nil)[:10]))
 			// Ignore the first 33 bytes
 			_, err = io.ReadFull(mwire, make([]byte, 32))
 			if err != nil {
@@ -56,6 +109,7 @@ func (cmd *Command) doProxy() {
 			if ctxTab[ctkey] == nil {
 				ctxTab[ctkey] = niaucchi2.NewServerCtx()
 				ctxTab[ctkey].Absorb(mwire)
+				go cmd.manageOneCtx(uid, ctxTab[ctkey])
 				go func() {
 					ctxTab[ctkey].Tomb().Wait()
 					ctxTabLok.Lock()
